3fffac6ca6e6ddb9c7fd216ce37a5e1fea1e4b65
chloe gregoire <chle.gregire@gmail.com>
2026-07-22 17:06:41 +0200

    fix import svg

--- src/components/play.tsx
+++ src/components/play.tsx
@@ -1,8 +1,11 @@
 
+import playIcon from '../assets/play.svg'
+
+
 export default function Play(){
   return (
     <div className= " hover-transition bg-[var(--red)] hover:bg-[var(--redhover)] px-2 rounded-lg flex items-center justify-center">
-      <img src="/src/assets/play.svg" alt="play_icon" className="w-[40px]" />
+      <img src={playIcon} alt="play_icon" className="w-[40px]" />
       <p>Lecture</p>
     </div>
   )
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,13 +1,16 @@
 import {Link} from 'react-router'
 import Nav from '../components/nav'
 
+import logo from "../assets/Logo.svg"
+
+
 export default function Header(){
   return (
     <div className="border border-[var(--line)] h-[4em] w-screen flex items-center fixed  lg:px-[2rem] px-[0.7rem] bg-[var(--bg)] z-100  ">
       <div className="flex gap-5 w-full items-center">
         <div>
           <Link to={'/'} className="w-fit ">
-            <img src="/src/assets/Logo.svg" alt="logo" height='auto' width='120' className='' />
+            <img src={logo} alt="logo" height='auto' width='120' className='' />
           </Link>
         </div>
         <Nav/>
